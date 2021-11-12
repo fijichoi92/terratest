456e32194792c4fb4ec5dad7aa54b1f5dcd9e3b0
khchoi <fijichoi92@gmail.com>
2021-11-12 12:22:57 +0900

    'step1'

---
+++ 13_albtg.tf
@@ -0,0 +1,22 @@
+resource "aws_lb_target_group" "khchoi_albtg" {
+  name      = "khchoi-albtg"
+  port      = 80
+  protocol  = "HTTP"
+  vpc_id    = aws_vpc.khchoi_vpc.id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 3
+    interval            = 5
+    matcher             = "200"
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 2
+    unhealthy_threshold = 2
+  }
+}
+
+output "alb_dns" {
+  value = aws_lb.khchoi_alb.dns_name
+}
