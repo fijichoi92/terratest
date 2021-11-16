200b84d08c479ebd107452ef65857f285a9a2db4
khchoi <fijichoi92@gmail.com>
2021-11-16 10:58:21 +0900

    'step2'

--- m06_atg.tf
+++ m06_atg.tf
@@ -8,13 +8,13 @@ resource "aws_ami_from_instance" "khchoi_ami" {
 }
 
 resource "aws_launch_configuration" "khchoi_aslc" {
-  name_prefix = "khchoi-web-"
-  image_id = aws_ami_from_instance.khchoi_ami.id
-  instance_type = var.default_instance
+  name_prefix          = "khchoi-web-"
+  image_id             = aws_ami_from_instance.khchoi_ami.id
+  instance_type        = var.default_instance
   iam_instance_profile = var.admin_role
-  security_groups = [aws_security_group.khchoi_sg.id]
-  key_name = var.key
-#  user_data =  없음으로 패스
+  security_groups      = [aws_security_group.khchoi_sg.id]
+  key_name             = var.key
+  #  user_data =  없음으로 패스
 
   #lifecycle {
   #  create_before_destroy = true
@@ -22,24 +22,24 @@ resource "aws_launch_configuration" "khchoi_aslc" {
 }
 
 resource "aws_placement_group" "khchoi_place" {
-  name      = "khchoi-place"
-  strategy  = var.place_str
+  name     = "khchoi-place"
+  strategy = var.place_str
 }
 
 resource "aws_autoscaling_group" "khchoi_asg" {
-  name = "khchoi-asg"
-  min_size  = var.asg_min
-  max_size  = var.asg_max
+  name                      = "khchoi-asg"
+  min_size                  = var.asg_min
+  max_size                  = var.asg_max
   health_check_grace_period = 10
-  health_check_type = "EC2"
-  desired_capacity  = var.asg_cap
-  force_delete = true
-  launch_configuration = aws_launch_configuration.khchoi_aslc.name
-  vpc_zone_identifier = [aws_subnet.khchoi_pub[0].id,aws_subnet.khchoi_pub[1].id]
+  health_check_type         = "EC2"
+  desired_capacity          = var.asg_cap
+  force_delete              = true
+  launch_configuration      = aws_launch_configuration.khchoi_aslc.name
+  vpc_zone_identifier       = [aws_subnet.khchoi_pub[0].id, aws_subnet.khchoi_pub[1].id]
 
- }
+}
 
 resource "aws_autoscaling_attachment" "khchoi_asgalbatt" {
   autoscaling_group_name = aws_autoscaling_group.khchoi_asg.id
-  alb_target_group_arn = aws_lb_target_group.khchoi_albtg.arn
+  alb_target_group_arn   = aws_lb_target_group.khchoi_albtg.arn
 }
