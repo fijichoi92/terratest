d50e454d5d1b22b88eb7cee0c03c3f041586ac9d
khchoi <fijichoi92@gmail.com>
2021-11-16 14:53:11 +0900

    'step1'

---
+++ m02_ig.tf
@@ -0,0 +1,24 @@
+# 10.0.0.0/16 VPC 전용 Internet Gateway
+resource "aws_internet_gateway" "khchoi_ig" {
+  vpc_id = aws_vpc.khchoi_vpc.id
+
+  tags = {
+    "Name" = "${var.name}_ig"
+  }
+}
+resource "aws_route_table" "khchoi_rt" {
+  vpc_id = aws_vpc.khchoi_vpc.id
+
+  route  {
+    cidr_block = var.cidr_route
+    gateway_id = aws_internet_gateway.khchoi_ig.id
+  }
+  tags = {
+    "Name" = "${var.name}-rt"
+  }
+}
+resource "aws_route_table_association" "khchoi_rtas"{
+  count          = length(var.cidr_public)
+  subnet_id      = aws_subnet.khchoi_pub[count.index].id
+  route_table_id = aws_route_table.khchoi_rt.id
+}
