9f8ac86bf18806a2724f732e074422bf85ce9805
khchoi <fijichoi92@gmail.com>
2021-11-16 14:05:37 +0900

    'step3'

--- m04_ec2.tf
+++ m04_ec2.tf
@@ -1,65 +1,65 @@
 resource "aws_security_group" "khchoi_sg" {
-  name        = "Allow Basic"
+  name = "Allow Basic"
   description = "Allow HTTP,SSH,SQL,ICMP"
-  vpc_id      = aws_vpc.khchoi_vpc.id
+  vpc_id = aws_vpc.khchoi_vpc.id
 
   ingress = [
     {
-      description      = "Allow ${var.protocol_http}"
-      from_port        = var.port_http
-      to_port          = var.port_http
-      protocol         = var.protocol_main
-      cidr_blocks      = [var.cidr_route]
-      ipv6_cidr_blocks = [var.cidr_ipv6]
-      prefix_list_ids  = null
-      security_groups  = null
-      self             = null
+      description       = "Allow ${var.protocol_http}"
+      from_port         = var.port_http
+      to_port           = var.port_http
+      protocol          = var.protocol_main
+      cidr_blocks       = [var.cidr_route]
+      ipv6_cidr_blocks  = [var.cidr_ipv6]
+      prefix_list_ids   = null
+      security_groups   = null
+      self              = null
     },
     {
-      description      = "Allow ${var.protocol_ssh}"
-      from_port        = var.port_ssh
-      to_port          = var.port_ssh
-      protocol         = var.protocol_main
-      cidr_blocks      = [var.cidr_route]
-      ipv6_cidr_blocks = [var.cidr_ipv6]
-      prefix_list_ids  = null
-      security_groups  = null
-      self             = null
+      description       = "Allow ${var.protocol_ssh}"
+      from_port         = var.port_ssh
+      to_port           = var.port_ssh
+      protocol          = var.protocol_main
+      cidr_blocks       = [var.cidr_route]
+      ipv6_cidr_blocks  = [var.cidr_ipv6]
+      prefix_list_ids   = null
+      security_groups   = null
+      self              = null
     },
     {
-      description      = "Allow ${var.protocol_sql}"
-      from_port        = var.port_sql
-      to_port          = var.port_sql
-      protocol         = var.protocol_main
-      cidr_blocks      = [var.cidr_route]
-      ipv6_cidr_blocks = [var.cidr_ipv6]
-      prefix_list_ids  = null
-      security_groups  = null
-      self             = null
+      description       = "Allow ${var.protocol_sql}"
+      from_port         = var.port_sql
+      to_port           = var.port_sql
+      protocol          = var.protocol_main
+      cidr_blocks       = [var.cidr_route]
+      ipv6_cidr_blocks  = [var.cidr_ipv6]
+      prefix_list_ids   = null
+      security_groups   = null
+      self              = null
     },
     {
-      description      = "Allow ${var.protocol_icmp}"
-      from_port        = var.port_zero
-      to_port          = var.port_zero
-      protocol         = var.protocol_icmp
-      cidr_blocks      = [var.cidr_route]
-      ipv6_cidr_blocks = [var.cidr_ipv6]
-      prefix_list_ids  = null
-      security_groups  = null
-      self             = null
+      description       = "Allow ${var.protocol_icmp}"
+      from_port         = var.port_zero
+      to_port           = var.port_zero
+      protocol          = var.protocol_icmp
+      cidr_blocks       = [var.cidr_route]
+      ipv6_cidr_blocks  = [var.cidr_ipv6]
+      prefix_list_ids   = null
+      security_groups   = null
+      self              = null
     }
   ]
   egress = [
     {
-      description      = "ALL"
-      from_port        = var.port_zero
-      to_port          = var.port_zero
-      protocol         = -1
-      cidr_blocks      = [var.cidr_route]
-      ipv6_cidr_blocks = [var.cidr_ipv6]
-      prefix_list_ids  = null
-      security_groups  = null
-      self             = null
+      description       = "ALL"
+      from_port         = var.port_zero
+      to_port           = var.port_zero
+      protocol          = -1
+      cidr_blocks       = [var.cidr_route]
+      ipv6_cidr_blocks  = [var.cidr_ipv6]
+      prefix_list_ids   = null
+      security_groups   = null
+      self              = null
     }
   ]
   tags = {
@@ -86,19 +86,19 @@ data "aws_ami" "amzn" {
 */
 
 resource "aws_instance" "khchoi_web" {
-  ami                    = var.ami_amazon_linux
-  instance_type          = var.default_instance
-  key_name               = var.key
-  availability_zone      = "${var.region}${var.ava[0]}"
-  private_ip             = var.private_ip
-  subnet_id              = aws_subnet.khchoi_pub[0].id
+  ami = var.ami_amazon_linux
+  instance_type = var.default_instance
+  key_name = var.key
+  availability_zone = "${var.region}${var.ava[0]}"
+  private_ip = var.private_ip
+  subnet_id = aws_subnet.khchoi_pub[0].id
   vpc_security_group_ids = [aws_security_group.khchoi_sg.id]
-  user_data              = file("./install.sh")
+  user_data = file("./install.sh")
 }
 
 resource "aws_eip" "khchoi_weba_ip" {
-  vpc                       = true
-  instance                  = aws_instance.khchoi_web.id
+  vpc = true
+  instance = aws_instance.khchoi_web.id
   associate_with_private_ip = var.private_ip
   depends_on = [
     aws_internet_gateway.khchoi_ig
